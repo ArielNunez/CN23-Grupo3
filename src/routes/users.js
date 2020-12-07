b775cb804122304411585552aab9581c13aa864b
eulupo <euge.porretti@gmail.com>
2020-12-07 13:19:58 -0300

    registro de usuarios con base de datos json

--- src/routes/users.js
+++ src/routes/users.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const usersController = require('../controllers/usersController');
 
 router.get('/registro', usersController.registro);
+router.post('/registro', usersController.guardar)
+
 router.get('/ingresar', usersController.ingresar);
 
 module.exports = router;
