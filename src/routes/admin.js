940685dbbedecff6f2bde99f86db635a85cbcfe2
santibullrich <santibullrich@gmail.com>
2020-12-10 20:53:27 -0300

    se agrega la ruta de admin

---
+++ src/routes/admin.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const path = require("path")
+const multer = require("multer")
+const productsController = require('../controllers/productsController');
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/img/uploads/productimage'));
+    },
+    filename: function (req, file, cb) {
+        console.log()
+        cb(null, req.body.idproducto + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+var upload = multer({ storage: storage })
+
+router.get('/productos/crear', productsController.crear);
+router.post('/productos/crear', upload.single("imagen"), productsController.crearProducto);
+
+module.exports = router;
