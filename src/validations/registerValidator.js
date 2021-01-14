9cd01f73fba2353a4e8037a105612e1a2b17e15d
eulupo <euge.porretti@gmail.com>
2021-01-14 11:11:52 -0300

    agrego algunos cambios a la validacion de registro

--- src/validations/registerValidator.js
+++ src/validations/registerValidator.js
@@ -2,21 +2,42 @@ const {check} = require('express-validator');
 
 module.exports = [
     check('nombre')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isLength({min:2, max:20})
-        .withMessage('Debés ingresar un nombre válido.'),
+        .withMessage('* Debés ingresar un nombre válido.'),
     check('apellido')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isLength({min:2, max:50})
-        .withMessage('Debés ingresar un apelido válido.'),
+        .withMessage('* Debés ingresar un apellido válido.'),
     check('nacimiento')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isDate()
-        .withMessage('Debés ingresar una fecha válida'),
+        .withMessage('* Debés ingresar una fecha válida.'),
     check('dni')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isLength({min:7, max: 8})
-        .withMessage('Debés ingresar un DNI válido'),
+        .withMessage('* Debés ingresar un DNI válido.'),
     check('email')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isEmail()
-        .withMessage('Debés ingresar un email válido.'),
+        .withMessage('* Debés ingresar un email válido.'),
+    check('confmail')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio'),
     check('pass')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio')
         .isLength({min:6, max:30})
-        .withMessage('La contraseña debe tener como mínimo 6 caracteres.')
+        .withMessage('* La contraseña debe tener como mínimo 6 caracteres.'),
+    check('confpass')
+        .notEmpty()
+        .withMessage('* Este campo es obligatorio'),
+    check('TyC')
+        .isIn(['si'])
+        .withMessage('* Debés aceptar los términos y condiciones antes de continuar.')
 ]
