40766b94469e8db240ae1e1fed896b6d90ec6980
ninasamartin <samartinnina@gmail.com>
2021-01-27 16:26:58 -0300

    arreglo de rutas

--- src/routes/users.js
+++ src/routes/users.js
@@ -15,10 +15,4 @@ router.post('/ingresar', loginValidator, usersController.logueado);
 
 router.get('/salir', authMiddleware, usersController.salir);
 
-router.get('/all', usersController.listado);
-
-router.get('/edit/:id', usersController.editar);
-router.post('/edit/:id', usersController.update);
-router.delete('/edit/:id', usersController.delete)
-
 module.exports = router;
