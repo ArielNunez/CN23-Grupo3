be118cedda700b9afb39d2724ec8287525207849
eulupo <euge.porretti@gmail.com>
2021-02-09 10:29:49 -0300

    login validator FRONT terminado

--- public/js/loginValidator.js
+++ public/js/loginValidator.js
@@ -1,27 +1,41 @@
-let form = document.querySelector('#login');
+window.addEventListener('load', function() {
 
-let email = document.querySelector('#email');
+    let form = document.querySelector('#login');
 
-let pass = document.querySelector('#pass');
+    let email = document.querySelector('#email');
+    let pass = document.querySelector('#pass');
 
-let smallMail = document.querySelector('#smallMail');
+    let smallMail = document.querySelector('#smallMail');
+    let smallPass = document.querySelector('#smallPass');
 
-let smallPass = document.querySelector('#smallPass');
+    let errores = {};
 
-let regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    let regEx = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-pass.addEventListener('blur', function() {
-    if (pass.value < 6) {
-        smallPass.innerHTML = 'La contraseña debe tener un mínimo de 6 caracteres'
-    } else {
-        smallPass.innerHTML = ''
-    }
-})
+    pass.addEventListener('blur', function() {
+        if (pass.value < 6) {
+            smallPass.innerHTML = '* La contraseña debe tener un mínimo de 6 caracteres';
+            errores.pass = true;
+        } else {
+            smallPass.innerHTML = '';
+            delete errores.pass;
+        }
+    });
 
-email.addEventListener('blur', function() {
-    if (!regEx.test(email.value)) {
-        smallMail.innerHTML = 'Debes ingresar un email correcto'
-    } else {
-        smallMail.innerHTML = ''
-    }
+    email.addEventListener('blur', function() {
+        if (!regEx.test(email.value)) {
+            smallMail.innerHTML = '* Debes ingresar un email correcto';
+            errores.email = true;
+        } else {
+            smallMail.innerHTML = '';
+            delete errores.email;
+        }
+    });
+
+    form.addEventListener('submit', function(event) {
+        if(Object.keys(errores).length != 0) {
+            event.preventDefault();
+        }
+    });
+
 })
--- src/validations/loginValidator.js
+++ src/validations/loginValidator.js
@@ -5,5 +5,5 @@ const { check, body } = require('express-validator');
 module.exports = [
     check('pass')
         .isLength({min: 6})
-        .withMessage('La contraseña debe tener mínimo 6 caracteres')
+        .withMessage('La contraseña debe tener un mínimo 6 caracteres')
 ]
