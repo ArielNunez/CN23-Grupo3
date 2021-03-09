5323223b845ba9f9d2dfc24ba415dfe6e799243d
santibullrich <santibullrich@gmail.com>
2021-03-09 15:09:18 -0300

    Agrego función buscar usuario por ID API

--- src/controllers/api/usersController.js
+++ src/controllers/api/usersController.js
@@ -13,5 +13,14 @@ module.exports = {
 .catch( (error) => {
     res.json(error);
 });
+},
+findById: function(req, res) {
+    db.Usuario.findByPk(req.params.id)
+    .then (function(usuario){
+        return res.status(200).json({usuario: usuario})
+    })
+    .catch( (error) => {
+        res.json(error);
+    })
 }
 }
--- src/routes/api/users.js
+++ src/routes/api/users.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const usersController = require('../../controllers/api/usersController');
 
 router.get('/', usersController.all);
+router.get('/:id', usersController.findById);
 
 module.exports = router;
