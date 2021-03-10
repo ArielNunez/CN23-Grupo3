6a2661114af91327e907d226225a157795804362
ninasamartin <samartinnina@gmail.com>
2021-03-09 21:07:23 -0300

    agrego productos similares en productDetail

--- public/js/navbars.js
+++ public/js/navbars.js
@@ -2,10 +2,14 @@ window.addEventListener('load', () => {
     /* Nav desplegable del header */
     let headerNav = document.querySelector('header .nav1 ul');
     let burger = document.querySelector('header .nav1 span');
+    let hr = document.querySelector('header .nav1 hr.perfil');
+    let perfil = document.querySelector('header .nav1 li.perfil');
 
     burger.addEventListener('click', () => {
         headerNav.classList.toggle('display-none');
         burger.classList.toggle('cross');
+        perfil.classList.toggle('display-none');
+        hr.classList.toggle('display-none');
     });
 
     /* Nav desplegable de edición de perfil */
