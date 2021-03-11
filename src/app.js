d12919d10eb56ae9879fe7018f7ad3ff4f027403
eulupo <euge.porretti@gmail.com>
2021-03-11 20:34:28 -0300

    cors

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const methodOverride = require('method-override');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 var SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const mainRouter = require('./routes/main');
@@ -28,6 +29,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride('_method'));
+app.use(cors())
 app.use(session({secret: 'ChampionesSecreto'}));
 app.use(loggedMiddleware);
 app.use(rememberMiddleware);
